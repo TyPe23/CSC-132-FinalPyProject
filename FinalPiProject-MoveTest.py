4259dd99383ac090fd086663a1feb1a6ed9ec1bc
Joshua Bailley <30305294+joshuab112@users.noreply.github.com>
2020-04-02 16:17:07 -0500

    Add files via upload
    Able to move an object up and down

---
+++ FinalPiProject-MoveTest.py
@@ -0,0 +1,78 @@
+import pygame
+import time
+from pygame.locals import *
+pygame.init()
+
+
+fps = 60
+
+clock = pygame.time.Clock()
+
+win = pygame.display.set_mode((480, 320))
+
+pygame.display.set_caption("First Game")
+
+LIME = ((180, 255, 100))
+PINK = ((255,100,180))
+TAN = ((230,220,170))
+BLACK = ((0, 0, 0))
+
+
+#rectangle that will be controlled by dpad
+player_rect = pygame.Rect(0, 0, 64, 64)
+
+#rectangle that will move the player_rect up
+up_rect = pygame.Rect(300, 100, 50, 50)
+
+#rectangle that will move the player_rect down
+down_rect = pygame.Rect(300, 180, 50, 50)
+
+running = True
+while running == True:
+
+	for event in pygame.event.get():
+		if event.type == pygame.QUIT:
+			running = False
+
+		#get the position of the mouse
+		mouse_pos = pygame.mouse.get_pos()
+
+		#check if the mouse has been pressed or not
+		click = pygame.mouse.get_pressed()
+
+		#if your mouse is over the up_rect and the left side of the mouse is clicked
+		if up_rect.collidepoint(mouse_pos) and click[0] == 1:
+			direction = "up"
+
+		#if your mouse is over the down_rect and the left side of the mouse is clicked	
+		elif down_rect.collidepoint(mouse_pos) and click[0] == 1:
+			direction = "down"
+
+		else:
+			direction = "stop"
+
+	#if the direction is up, move the y position up
+	if direction == "up":
+		player_rect.y -= 3
+	
+	#if the direction is down, move the y position down
+	elif direction == "down":
+		player_rect.y += 3
+
+	#if the direction is stop, do not move 
+	elif direction == "stop":
+		player_rect.y += 0
+
+
+	#fill the window with the color black
+	win.fill(BLACK)
+
+	#creating the rectangles 
+	pygame.draw.rect(win, PINK, player_rect)
+	pygame.draw.rect(win, TAN, up_rect)
+	pygame.draw.rect(win, LIME, down_rect)
+
+	clock.tick(fps)
+	pygame.display.update()
+
+pygame.quit()
