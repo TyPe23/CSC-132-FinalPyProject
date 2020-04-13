11918e8973b2c4ee1a06ed45df755c78bedf19ad
TyPe23 <62142901+TyPe23@users.noreply.github.com>
2020-04-13 17:22:09 -0500

    Add files via upload

--- FinalPyProject.py
+++ FinalPyProject.py
@@ -1,5 +1,5 @@
 import pygame
-pygame.init
+pygame.init()
 
 winWidth = 500
 winHeight = 500
@@ -7,35 +7,68 @@ x = winWidth - winWidth / 2
 y = winHeight - winHeight / 4
 playerWidth = 128
 playerHeight = 128
-vel = 10
+vel = 5
 left = False
 right = False
-walkCount = 0
 idleCount = 0
+bgCount = 0
+bgCount2 = -1235
 
 win = pygame.display.set_mode((winWidth, winHeight))
 
 pygame.display.set_caption("Final Py Project")
 
+bullet = pygame.image.load("sprites/Bullets.png")
 bg = pygame.image.load("sprites/City_Background.png")
 char = [pygame.image.load("sprites/Player_Ship.png"), pygame.image.load("sprites/Player_Ship2.png")]
+charLeft = [pygame.image.load("sprites/Player_Ship_Left.png"), pygame.image.load("sprites/Player_Ship_Left2.png")]
+charRight = [pygame.image.load("sprites/Player_Ship_Right.png"), pygame.image.load("sprites/Player_Ship_Right2.png")]
 
 clock = pygame.time.Clock()
 
+class Bullet(pygame.sprite.Sprite):
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        win.blit(bullet, (x, y - 50))
+
+    def update(self, x, y):
+        win.blit(bullet, (x, y -50))
+        self.y -= 5
+
+def shoot():
+    bullet = Bullet(x, y)
+    bullets.add(bullet)
+
+bullets = pygame.sprite.Group()
+
 def drawWin():
-    global walkCount
+    global bgCount
+    global bgCount2
     global idleCount
 
-    win.blit(bg, (0,0))
+    win.blit(bg, (0, bgCount))
+    win.blit(bg, (0, bgCount2))
+    bgCount += 5
+    bgCount2 += 5
+    if (bgCount >= 500):
+        bgCount = bgCount2 - 1235
 
-    if (walkCount + 1 >= 6):
-        walkCount = 0
+    if (bgCount2 >= 500):
+        bgCount2 = bgCount -1235
 
-    if (idleCount + 1 >= 6):
+    if (idleCount + 1 >= 12):
         idleCount = 0
 
-    win.blit(char[idleCount // 3], (x, y))
+    if (left == True):
+        win.blit(charLeft[idleCount // 6], (x, y))
+    elif (right == True):
+        win.blit(charRight[idleCount // 6], (x, y))
+    else:
+        win.blit(char[idleCount // 6], (x, y))
     idleCount += 1
+
+    bullets.update(x, y)
 
     pygame.display.update()
 
@@ -43,7 +76,7 @@ def drawWin():
 run = True
 
 while (run):
-    clock.tick(27)
+    clock.tick(60)
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
